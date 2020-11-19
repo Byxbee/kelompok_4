4c0842379cec737037d0c7b52090cc168cca031b
zulqarnain <zulqarnain.hidayat@yahoo.com>
2020-11-19 15:13:53 +0700

    update card

--- lib/screenFix.dart
+++ lib/screenFix.dart
@@ -37,13 +37,6 @@ class _screenFixState extends State<screenFix> {
                       elevation: 8,
                       margin: EdgeInsets.only(bottom: 10, top: 20, left: 20, right: 20),
                       child: ListTile(
-                        onTap: () => {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(builder: (context)=>screen4()
-                              ),
-                          )
-                        },
-
                       leading: Container(
                         padding: EdgeInsets.only(right: 12),
                         decoration: BoxDecoration(
@@ -51,38 +44,43 @@ class _screenFixState extends State<screenFix> {
                             right: BorderSide(width: 1, color: Colors.brown),
                           ),
                         ),
-                        child: CircleAvatar(
-                          // child: Text('1'),
-                          backgroundColor: Colors.blue,
+                        // child: CircleAvatar(
+                        //   // child: Text('1'),
+                        //   backgroundColor: Colors.blue,
+                        // ),
+                        child: Icon(
+                          Icons.book,
+                          color: Colors.pinkAccent,
+                          size: 70,
                         ),
                       ),
 
-                        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                         title: Text(
-                          'Judul 1',
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                          'Materi Kelas X SMA',
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
                         ),
                         subtitle: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
-                            Row(
-                              children: <Widget>[
-                                Icon(
-                                  Icons.local_florist,
-                                  color: Colors.pinkAccent,
-                                ),
-                                Expanded(child: Text('Kelas X SMA')),
-                              ],
-                            ),
-                            Row(
-                              children: <Widget>[
-                                Icon(
-                                  Icons.local_florist,
-                                  color: Colors.pinkAccent,
-                                ),
-                                Text('Sumber Tahun 2017'),
-                              ],
-                            ),
+                            // Row(
+                            //   children: <Widget>[
+                            //     Icon(
+                            //       Icons.book,
+                            //       color: Colors.pinkAccent,
+                            //     ),
+                            //     Expanded(child: Text('Kelas X SMA')),
+                            //   ],
+                            // ),
+                            // Row(
+                            //   children: <Widget>[
+                            //     Icon(
+                            //       Icons.local_florist,
+                            //       color: Colors.pinkAccent,
+                            //     ),
+                            //     Text('Sumber Tahun 2017'),
+                            //   ],
+                            // ),
                             Column(
                               children: <Widget> [
                                 FlatButton(
@@ -91,13 +89,15 @@ class _screenFixState extends State<screenFix> {
                                       kelas10 = !kelas10;
                                     });
                                   },
-                                  child: Text('Read more'),
+                                  child: Text(
+                                    'Lihat materi',
+                                    style: TextStyle(fontSize: 16),
+                                  ),
                                 ),
                               ],
                             ),
                           ],
                         ),
-                          trailing: Icon(Icons.keyboard_arrow_right,size: 30,),
                         ),
                     ),
                   ),
@@ -106,6 +106,7 @@ class _screenFixState extends State<screenFix> {
                   if(kelas10)Container(
                   child: Column(
                     children: [
+                      // card biru 1
                       Card(
                           color: Colors.blueGrey,
                           elevation: 8,
@@ -151,6 +152,7 @@ class _screenFixState extends State<screenFix> {
                               ),
                             ),
                       ),
+                      // card biru 2
                       Card(
                         color: Colors.blueGrey,
                         elevation: 8,
@@ -158,7 +160,7 @@ class _screenFixState extends State<screenFix> {
                         child: ListTile(
                           onTap: () => {
                             Navigator.of(context).push(
-                              MaterialPageRoute(builder: (context)=>screen4()
+                              MaterialPageRoute(builder: (context)=>screen5()
                                 ),
                             )
                           },
@@ -211,13 +213,6 @@ class _screenFixState extends State<screenFix> {
                       elevation: 8,
                       margin: EdgeInsets.only(bottom: 10, top: 20, left: 20, right: 20),
                       child: ListTile(
-                        onTap: () => {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(builder: (context)=>screen4()
-                              ),
-                          )
-                        },
-
                       leading: Container(
                         padding: EdgeInsets.only(right: 12),
                         decoration: BoxDecoration(
@@ -225,38 +220,43 @@ class _screenFixState extends State<screenFix> {
                             right: BorderSide(width: 1, color: Colors.brown),
                           ),
                         ),
-                        child: CircleAvatar(
-                          // child: Text('1'),
-                          backgroundColor: Colors.blue,
+                        // child: CircleAvatar(
+                        //   // child: Text('1'),
+                        //   backgroundColor: Colors.blue,
+                        // ),
+                        child: Icon(
+                          Icons.book,
+                          color: Colors.pinkAccent,
+                          size: 70,
                         ),
                       ),
 
-                        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                         title: Text(
-                          'Judul 1',
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                          'Materi Kelas XI SMA',
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
                         ),
                         subtitle: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
-                            Row(
-                              children: <Widget>[
-                                Icon(
-                                  Icons.local_florist,
-                                  color: Colors.pinkAccent,
-                                ),
-                                Expanded(child: Text('Kelas X SMA')),
-                              ],
-                            ),
-                            Row(
-                              children: <Widget>[
-                                Icon(
-                                  Icons.local_florist,
-                                  color: Colors.pinkAccent,
-                                ),
-                                Text('Sumber Tahun 2017'),
-                              ],
-                            ),
+                            // Row(
+                            //   children: <Widget>[
+                            //     Icon(
+                            //       Icons.local_florist,
+                            //       color: Colors.pinkAccent,
+                            //     ),
+                            //     Expanded(child: Text('Kelas X SMA')),
+                            //   ],
+                            // ),
+                            // Row(
+                            //   children: <Widget>[
+                            //     Icon(
+                            //       Icons.local_florist,
+                            //       color: Colors.pinkAccent,
+                            //     ),
+                            //     Text('Sumber Tahun 2017'),
+                            //   ],
+                            // ),
                             Column(
                               children: <Widget> [
                                 FlatButton(
@@ -265,13 +265,15 @@ class _screenFixState extends State<screenFix> {
                                       kelas11 = !kelas11;
                                     });
                                   },
-                                  child: Text('Read more'),
+                                  child: Text(
+                                    'Lihat materi',
+                                    style: TextStyle(fontSize: 16),
+                                  ),
                                 ),
                               ],
                             ),
                           ],
                         ),
-                          trailing: Icon(Icons.keyboard_arrow_right,size: 30,),
                         ),
                     ),
                   ),
@@ -280,6 +282,7 @@ class _screenFixState extends State<screenFix> {
                   if(kelas11)Container(
                   child: Column(
                     children: [
+                      //card biru 1
                       Card(
                           color: Colors.blueGrey,
                           elevation: 8,
@@ -325,6 +328,7 @@ class _screenFixState extends State<screenFix> {
                               ),
                             ),
                       ),
+                      //card biru 2
                       Card(
                         color: Colors.blueGrey,
                         elevation: 8,
